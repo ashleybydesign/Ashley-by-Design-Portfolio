be02fcd88ecedaac279bb19f96521968cda65a8d
ashleybydesign <ash.mariehe@gmail.com>
2023-10-07 18:09:52 -0400

    final update

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 function toggleMenu() {
-    var x = document.getElementById('myNavtoggle');
-    if (x.style.display === 'block') {
-      x.style.display = 'none';
-    } else {
-      x.style.display = 'block';
-    }
+  var x = document.getElementById("myNavtoggle");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
+}
 
 console.log("Hi! This is my portfolio!");
 
-  
+
